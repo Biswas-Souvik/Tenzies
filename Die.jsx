e96e319f0ace03cd217bd820124d912b87b19f4d
Biswas-Souvik <biswassouvik284@gmail.com>
2025-06-16 12:29:39 +0530

    added new game button

--- Die.jsx
+++ Die.jsx
@@ -2,6 +2,7 @@ export default function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? "#59E391" : "white",
   };
+
   return (
     <button style={styles} onClick={props.hold}>
       {props.value}
