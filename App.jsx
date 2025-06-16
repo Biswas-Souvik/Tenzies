e32062f5d7a678aa36b2f3424ef9d2b5cd45e521
Biswas-Souvik <biswassouvik284@gmail.com>
2025-06-16 12:15:13 +0530

    hold dices after roll

--- App.jsx
+++ App.jsx
@@ -14,15 +14,19 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice(generateAllNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+      )
+    );
   }
 
   function hold(id) {
-    setDice((oldDice) => {
-      return oldDice.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-      });
-    });
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
   }
 
   const diceElements = dice.map((dieObj) => (
